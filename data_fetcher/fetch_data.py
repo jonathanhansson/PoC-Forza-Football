2bdf40f033657c21ef009b7b665c378e4a62f825
jonathanhansson <jonathanhanssondg@gmail.com>
2025-03-21 08:52:13 +0100

    Folder and furthermore file that fetches data from the API

---
+++ data_fetcher/fetch_data.py
@@ -0,0 +1,16 @@
+import requests
+
+api_key = "e547ef531e5848919c3dfe43d910d435"
+url = "https://api.football-data.org/v4/competitions/PL/matches"
+
+headers = {
+    "X-Auth-Token": api_key
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"Failed to fetch data. Error code: {response.status_code}")
