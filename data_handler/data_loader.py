a1875613e4b2f2d14dbf01ae43252f536dfc06d4
jonathanhansson <jonathanhanssondg@gmail.com>
2025-03-23 13:48:25 +0100

    Minor fixes in data_loader

--- data_handler/data_loader.py
+++ data_handler/data_loader.py
@@ -2,6 +2,12 @@ import zipfile
 import pandas as pd
 import os
 
+#======================================================
+# The "DataLoader"-class has a function which runs the pipeline, which is "process_data"
+# The "DataLoader"-class also has a function which extracts zip-files from the dataset
+# The "DataLoader"-class, when instan (__init__), also creates a new folder which then contains the extracted data in csv-files which are readable for Pandas 
+#======================================================
+
 class DataLoader:
     def __init__(self, zip_folder: str, extract_folder: str = "extracted_data"):
         self.zip_folder = zip_folder
@@ -20,12 +26,12 @@ class DataLoader:
         dataframes = {}
 
         for file in os.listdir(self.extract_folder):
-            if file.endswith("csv"):
+            if file.endswith(".csv"):
                 file_path = os.path.join(self.extract_folder, file)
                 df = pd.read_csv(file_path)
                 dataframes[file] = df
                 print(f"Laddade in: {file} ({df.shape[0]} rader, {df.shape[1]} kolumner)")
-            return dataframes
+        return dataframes
     
     def process_data(self):
         self.extract_zip_files()
