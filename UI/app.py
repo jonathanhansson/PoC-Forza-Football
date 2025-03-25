f3cbe13c3b4caf45f72370c1f5b5d14423018824
jonathanhansson <jonathanhanssondg@gmail.com>
2025-03-25 18:54:19 +0100

    The UI has changed, now only displays the model line

--- UI/app.py
+++ UI/app.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import plotly.express as px
 import streamlit as st
 import pandas as pd
 import sys
@@ -27,13 +26,10 @@ def load_predictions():
     return y_test, y_pred
 
 def plot_graph(y_test, y_pred):
-    plt.figure(figsize=(15, 15))
+    plt.figure(figsize=(10, 6))
 
     plt.scatter(y_test, y_pred, color="blue", alpha=0.5, label="Predicted values")
-    
-    min_val = min(y_test.min(), y_pred.min())  # Gemensamt min
-    max_val = max(y_test.max(), y_pred.max())  # Gemensamt max
-    plt.plot([min_val, max_val], [min_val, max_val], 'r--', label="Perfect line")   
+    plt.plot([y_test.min(), y_test.max()], [y_pred.min(), y_pred.max()], color="red", linestyle="-", label="The perfect prediction line")
 
     plt.legend()
 
@@ -46,31 +42,12 @@ def plot_graph(y_test, y_pred):
 
     st.pyplot(plt)
 
-def plot_radar(player_name, df):
-    player_stats = df[df["Player"] == player_name][["Goals_90_z", "Assists_90_z", "Tackle%_z", "Interceptions_z"]]
-    fig = px.line_polar(
-        player_stats, 
-        r=player_stats.values[0], 
-        theta=player_stats.columns,
-        line_close=True,
-        color_discrete_sequence=["#FF6B6B"],  # Forza-färger? 😉
-        title=f"Spelarprofil: {player_name}"
-    )
-    fig.update_layout(paper_bgcolor="rgba(0,0,0,0)")  # Genomskinlig bakgrund
-    st.plotly_chart(fig)
-
 def main():
     st.title("Player rating prediction comparison")
 
-    players_csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Players.csv"))
-    df = pd.read_csv(players_csv_path)
-
     y_test, y_pred = load_predictions()
 
-
     plot_graph(y_test, y_pred)
-    selected_player = st.selectbox("Välj spelare: ", df["Key"].str[10:])
-    plot_radar(selected_player, df)
 
 if __name__ == "__main__":
     main()
