af582a47b84a598a44ad66c96051afce24a9ae23
jonathanhansson <jonathanhanssondg@gmail.com>
2025-03-27 09:21:27 +0100

    Easier file path to the finished CSV

--- UI/app.py
+++ UI/app.py
@@ -102,7 +102,7 @@ def plot_graph(y_test, y_pred):
 
 @st.cache_data
 def load_and_filter_data():
-    df = pd.read_csv(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Players.csv")))
+    df = pd.read_csv("../Players.csv")
     return df
 
 
